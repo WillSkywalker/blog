b1ddf49f011e9d024575448caa28082569298e1d
Will Skywalker <cxbats@gmail.com>
2016-05-19 20:43:19 +0800

    last call

--- config.py
+++ config.py
@@ -2,10 +2,11 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.getenv('SS') or 'kuchtopatazindagis'
+    f = open('.secrets').readlines()
+    SECRET_KEY = f[0] or 'kuchtopatazindagis'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    BLOG_ADMIN = os.environ.get("BLOG_ADMIN")
-    BLOG_PASSWD = os.environ.get('BLOG_PASSWD')
+    BLOG_ADMIN = f[1] or os.environ.get("BLOG_ADMIN")
+    BLOG_PASSWD = f[2] or os.environ.get('BLOG_PASSWD')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     FREEZER_BASE_URL = '/blog'
