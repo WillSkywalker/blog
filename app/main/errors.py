38b47662a6480baa5cc9c4fe2a7981e835af9a1b
Will Skywalker <cxbats@gmail.com>
2016-05-19 09:59:10 +0800

    final approach

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,10 +1,10 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, request
 from . import main
 
 @main.app_errorhandler(404)
 def not_found(e):
     print(e)
-    return '40404 040404 !!!'
+    return render_template('404.html')
 
 @main.app_errorhandler(500)
 def internal_error(e):
