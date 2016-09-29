94bad6de64bb2ad393d7c464ac6825a68a1dd391
Will Skywalker <cxbats@gmail.com>
2016-09-29 09:23:45 +0000

    Fuck sqlite!

---
+++ migrations/versions/148d1dcc5fec_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 148d1dcc5fec
+Revises: ('062056abab41', '58e5d49cb165')
+Create Date: 2016-09-29 08:41:09.439707
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '148d1dcc5fec'
+down_revision = ('062056abab41', '58e5d49cb165')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
